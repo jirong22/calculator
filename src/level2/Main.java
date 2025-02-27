8afedb6ffadd8367e6daefc6ef9177d6f3f32009
jirong22 <qkrwldus995@gmail.com>
2025-02-27 11:20:37 +0900

    계산기 init

--- src/level2/Main.java
+++ src/level2/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-
 
         while (true) {
             System.out.print("1번째 숫자를 입력하세요: ");
             int num1 = scanner.nextInt();
+            calculator.setNum1(num1); //
 
             System.out.print("2번째 숫자를 입력하세요: ");
             int num2 = scanner.nextInt();
+            calculator.setNum2(num2);
 
-            System.out.print("사용할 연산자를 입력하세요: ");
+            System.out.print("사용할 연산자를 입력하세요(+,-,*,/): ");
             char operator = scanner.next().charAt(0);
 
             //결과초기값설정
@@ -24,18 +24,22 @@ public class Main {
 
             switch (operator) {
                 case '+':
-                    result = calculator.add(num1, num2);
+                    result = calculator.add();
                     break;
 
                 case '-':
-                    result = calculator.sub(num1, num2);
+                    result = calculator.sub();
                     break;
 
                 case '*':
-                    result = calculator.mul(num1, num2);
+                    result = calculator.mul();
                     break;
 
                 case '/':
+                    if (num2 == 0) {
+                        System.out.println("0으로 나눌수없습니다.");
+                        continue; //다시시작
+                    }
                     result = calculator.div(num1, num2);
                     break;
 
@@ -45,18 +49,14 @@ public class Main {
             }
 
             //결과출력
-
             System.out.println("결과 :  " + result);
 
             System.out.print("exit 입력시종료 :");
             String exitInput = scanner.next();
-
             if (exitInput.equals("exit")) {
-                System.out.println("계산종료");
-                break; // 종료
+                break;
             } else {
                 System.out.println("잘못된입력입니다.");
-
             }
         }
     }
