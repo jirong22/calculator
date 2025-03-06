c78edf84c1635d169ddeaa74e80c3e5abb3bd24f
jirong22 <qkrwldus995@gmail.com>
2025-03-06 13:56:23 +0900

    feat: 연산 결과 저장 및 getter/setter 리팩토링

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -1,42 +1,81 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
+    //캡슐화
     private int num1;
     private int num2;
 
-    //num1캡슐 세터
+    //연산결과 리스크에 저장하도록 리스트 추가
+    private List<Integer> history;
+
+
+    public Calculator(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.history = new ArrayList<>();
+    }
+
+    // 값을 반환 게터
+    public int getNum1() {
+        return num1;
+    }
+    public int getNum2() {
+        return num2;
+    }
+
+    // 값을 추가 변경 세터
     public void setNum1(int num1) {
         this.num1 = num1;
     }
-    //num2캡슐설정
     public void setNum2(int num2) {
         this.num2 = num2;
     }
 
+    // 연산결과 저장 int result
+    private void saveResult(int result) {
+        history.add(result);
+    }
+
+    // 리스트 전체 반환
+    public  List<Integer> getHistory() {
+        return history;
+    }
+
     //덧셈
     public int add() {
-        return num1 + num2;
+        int result = num1 + num2;
+        saveResult(result);
+        return result;
     }
 
     //뺄셈
     public int sub() {
-        return  num1 - num2;
+        int result = num1 - num2;
+        saveResult(result);
+        return result;
     }
 
     //곱셈
     public int mul() {
-        return  num1 * num2;
+        int result = num1 * num2;
+        saveResult(result);
+        return  result;
     }
 
     //나눗셈
-    public double div(int a, int b) {
+    public double div() {
         if (num2 == 0) {
             System.out.println("0으로 나눌수 없습니다."); //0일경우만
             return 0;
         }
-        return (double) num1 / num2;
+       //정수로 변환후 저장
+        double result = (double) num1 / num2;
+        saveResult((int) result);
+        return result;
     }
 
 }
--- src/level2/Main.java
+++ src/level2/Main.java
@@ -1,65 +1,73 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    // 연산기록 저장
+    private static List<Integer> history = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
 
-        while (true) {
-            System.out.print("1번째 숫자를 입력하세요: ");
-            int num1 = scanner.nextInt();
-            calculator.setNum1(num1); //
+            while (true) {
+                System.out.print("1번째 숫자를 입력하세요: ");
+                int num1 = scanner.nextInt();
+                history.add(num1);
 
-            System.out.print("2번째 숫자를 입력하세요: ");
-            int num2 = scanner.nextInt();
-            calculator.setNum2(num2);
 
-            System.out.print("사용할 연산자를 입력하세요(+,-,*,/): ");
-            char operator = scanner.next().charAt(0);
+                System.out.print("2번째 숫자를 입력하세요: ");
+                int num2 = scanner.nextInt();
+                history.add(num2);
+                System.out.print("사용할 연산자를 입력하세요(+,-,*,/): ");
+                char operator = scanner.next().charAt(0);
 
-            //결과초기값설정
-            double result = 0;
+                //결과초기값설정
+                double result = 0;
 
-            switch (operator) {
-                case '+':
-                    result = calculator.add();
+                switch (operator) {
+                    case '+':
+                        result = history.get(history.size() - 1) + history.get(history.size() - 2);
+                        break;
+
+                    case '-':
+                        result = history.get(history.size() - 1) - history.get(history.size() - 2);
+                        break;
+
+                    case '*':
+                        result = history.get(history.size() - 1) * history.get(history.size() - 2);
+                        break;
+
+                    case '/':
+                        if (num2 == 0) {
+                            System.out.println("0으로 나눌수없습니다.");
+                            continue; //다시시작
+                        }
+                        result = (double) history.get(history.size() - 2) / history.get(history.size() - 1);
+                        break;
+
+                    default:
+                        System.out.println("잘못된 계산입니다.");
+                        continue;
+                }
+
+                //결과출력
+                System.out.println("결과 :  " + result);
+
+                System.out.print("exit 입력시종료 :");
+                String exitInput = scanner.next();
+                if (exitInput.equals("exit")) {
                     break;
-
-                case '-':
-                    result = calculator.sub();
-                    break;
-
-                case '*':
-                    result = calculator.mul();
-                    break;
-
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("0으로 나눌수없습니다.");
-                        continue; //다시시작
-                    }
-                    result = calculator.div(num1, num2);
-                    break;
-
-                default:
-                    System.out.println("잘못된 계산입니다.");
-                    continue;
-            }
-
-            //결과출력
-            System.out.println("결과 :  " + result);
-
-            System.out.print("exit 입력시종료 :");
-            String exitInput = scanner.next();
-            if (exitInput.equals("exit")) {
-                break;
-            } else {
-                System.out.println("잘못된입력입니다.");
+                } else {
+                    System.out.println("잘못된입력입니다.");
+                }
             }
         }
+        public static List<Integer> getHistory() {
+            return history;
+        }
     }
-}
 
 
